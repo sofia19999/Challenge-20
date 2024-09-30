2c200c30e585348f260bd4cb3f9b83ec418690c2
sofia19999 <kaminskisofia9@gmail.com>
2024-09-30 14:14:06 -0400

    Initial setup of React portfolio project

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, welcome to my React App!</h1>
+      <p>This is a simple portfolio page.</p>
+    </div>
+  );
+}
+
+export default App;
